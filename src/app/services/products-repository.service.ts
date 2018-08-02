1dbbc72fdf39a0be547da127699c7ff7f2f33ba8
Frank Javier <frankjavier@Franks-MacBook-Pro.local>
2018-08-02 08:12:06 -0500

    Add changes

--- src/app/services/products-repository.service.ts
+++ src/app/services/products-repository.service.ts
@@ -18,23 +18,29 @@ export class ProductsRepositoryService {
 
   async start() {
     this.products = [];
-    const querySnapshot = await this.db.collection('products').get();
-    querySnapshot.forEach(doc => {
-      this.products.push({
-        id: doc.id,
-        name: doc.data().shortDescription,
-        description: doc.data().longDescription,
-        price: doc.data().price,
-        imageUrl: `https://super.walmart.com.mx/images/product-images/img_medium/${doc.data().imageId}m.jpg`
+
+    if (window.localStorage.products) {
+      this.products = JSON.parse(window.localStorage.products);
+    } else {
+      const querySnapshot = await this.db.collection('products').get();
+      querySnapshot.forEach(doc => {
+        this.products.push({
+          id: doc.id,
+          name: doc.data().shortDescription,
+          description: doc.data().longDescription,
+          price: doc.data().price,
+          imageUrl: `https://super.walmart.com.mx/images/product-images/img_medium/${doc.data().imageId}m.jpg`
+        });
       });
-    });
+      window.localStorage.products = JSON.stringify(this.products);
+    }
 
     this.createIndex();
   }
 
   search(query: string): Product[] {
     if (query == null) {
-      return this.products;
+      return [];
     }
 
     let results = this.index.search(query,  {
